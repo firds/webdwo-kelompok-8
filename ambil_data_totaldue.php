ea1266616b054ad3038e12646933ae347d003311
firds <dmasamune94@gmail.com>
2023-06-15 02:12:10 +0700

    init

---
+++ ambil_data_totaldue.php
@@ -0,0 +1,20 @@
+<?php
+include_once('koneksi.php');
+
+// Query untuk mengambil data dari tabel fakta_penjualan
+$query = "select concat(w.Tahun, ' ', monthname(w.Tanggal)) as tanggal, 
+sum(fp.TotalDue) as totaldue
+from `fakta penjualan` fp
+join waktu w on w.time_id = fp.time_id 
+group by year(w.Tanggal), month(w.Tanggal);";
+$result = mysqli_query($koneksi, $query);
+
+// Simpan hasil query ke dalam array
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+
+// Mengirim data dalam format JSON
+echo json_encode($data);
+?>
